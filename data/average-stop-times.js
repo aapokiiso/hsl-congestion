e039cdada506f4745dd92ed89e7019388343f9ec
Aapo Kiiso <aapo@kii.so>
2017-04-23 15:00:51 +0000

    Tweaks to average stop time script

--- data/average-stop-times.js
+++ data/average-stop-times.js
@@ -11,14 +11,20 @@ sequelize.init()
         seq.models.Stop.findAll()
     ]))
     .then(function ([positions, stops]) {
-        const vehicleIds = positions.filter((position, i, arr) => arr.indexOf(position) === i);
+        const vehicleIds = positions
+            .map(pos => pos.get('vehicleId'))
+            .filter((vehicleId, i, arr) => arr.indexOf(vehicleId) === i);
 
         vehicleIds.forEach(function (vehicleId) {
             const vehiclePositions = positions
                 .filter(pos => pos.get('vehicleId') == vehicleId);
 
             const stopPositions = vehiclePositions.reduce((acc, val) => {
-                const posCoords = {latitude: val.get('latitude'), longitude: val.get('longitude')};
+                const posCoords = {
+                    latitude: val.get('latitude'), 
+                    longitude: val.get('longitude')
+                };
+
                 const closestStop = stops.sort((a, b) => {
                     const distA = geolib.getDistance(posCoords, {latitude: a.get('latitude'), longitude: a.get('longitude')});
                     const distB = geolib.getDistance(posCoords, {latitude: b.get('latitude'), longitude: b.get('longitude')});
@@ -26,7 +32,11 @@ sequelize.init()
                     return distA <= distB ? -1 : 1;
                 })[0];
 
-                const stopCoords = {latitude: closestStop.get('latitude'), longitude: closestStop.get('longitude')};
+                const stopCoords = {
+                    latitude: closestStop.get('latitude'), 
+                    longitude: closestStop.get('longitude')
+                };
+
                 if (geolib.getDistance(posCoords, stopCoords) < stopRange) {
                     if (!acc[closestStop.get('id')]) {
                         acc[closestStop.get('id')] = [];
@@ -34,15 +44,24 @@ sequelize.init()
 
                     acc[closestStop.get('id')].push(val);
                 }
+
+                return acc;
             }, {});
 
-            const stopTimes = stopPositions.map(positions => {
-                const sortedPositions = positions
+            const stopTimes = {};
+            for (let stopId in stopPositions) {
+                if (!stopPositions.hasOwnProperty(stopId)) {
+                    continue;
+                }
+
+                const sortedPositions = stopPositions[stopId]
                     .sort((a, b) => a.get('timestamp') <= b.get('timestamp') ? -1 : 1);
 
-                return sortedPositions[sortedPositions.length - 1].get('timestamp') - sortedPositions[0].get('timestamp');
-            });
+                stopTimes[stopId] = sortedPositions[sortedPositions.length - 1].get('timestamp') - sortedPositions[0].get('timestamp');
+            }
 
             console.log(stopTimes);
         });
-    });
+    })
+    .catch(err => console.error(err));
+
