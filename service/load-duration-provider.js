1d7ed5adf2e6a45aee646ec3fd081d8a48e2fdd7
Aapo Kiiso <aapo@lamia.fi>
2019-08-20 21:03:38 +0300

    Refactor load time calc to work with HFP v2

--- service/load-duration-provider.js
+++ service/load-duration-provider.js
@@ -38,42 +38,104 @@ module.exports = {
 
 function sumLoadDurationFromTimestampsLog(timestampsLog) {
     return timestampsLog
-        .reduce(groupTimestampsByDoorStatusReducer, [])
-        .map(mapTimestampGroupDurationWithDoorStatus)
-        .filter(timestampGroup => timestampGroup.doorsOpen)
-        .reduce(sumTimestampGroupDurationsReducer, 0);
+        .filter(removeAdjacentDuplicateTimestampsFilter)
+        .reduce(groupTimestampsByDoorStatusIntervalReducer, [])
+        .filter(removeClosedDoorTimestampGroupsFilter)
+        .map(timestampGroupToDoorStatusDurationMapper)
+        .reduce(sumDoorStatusDurationsReducer, 0);
 }
 
-function groupTimestampsByDoorStatusReducer(timestampGroups, currentTimestamp) {
-    const [previousGroup] = timestampGroups.slice().reverse();
+/**
+ * Remove possible adjacent timestamps which have the same door status,
+ * so that pairs of open-closed door timestamps can be formed reliably.
+ *
+ * @param {Object} timestamp
+ * @param {number} idx
+ * @param {Array<Object>} timestamps
+ * @returns {boolean}
+ */
+function removeAdjacentDuplicateTimestampsFilter(timestamp, idx, timestamps) {
+    return !timestamps[idx - 1] || timestamp.doorsOpen !== timestamps[idx - 1].doorsOpen;
+}
 
-    if (previousGroup && previousGroup.doorsOpen === currentTimestamp.doorsOpen) {
-        previousGroup.timestamps.push(currentTimestamp);
-    } else {
+/**
+ * Pair open-doored timestamp to closed-doored timestamp (and vice versa),
+ * so time difference between them can be measured.
+ *
+ * @param {Array<Object>} timestampGroups
+ * @param {Object} currentTimestamp
+ * @returns {Array<Object>}
+ */
+function groupTimestampsByDoorStatusIntervalReducer(timestampGroups, currentTimestamp) {
+    const [latestGroup] = timestampGroups.slice(-1);
+
+    if (!latestGroup) {
+        // Initialize first pair
         timestampGroups.push({
+            tripId: currentTimestamp.tripId,
             doorsOpen: currentTimestamp.doorsOpen,
             timestamps: [currentTimestamp],
         });
+
+        return timestampGroups;
     }
+
+    if (latestGroup.tripId === currentTimestamp.tripId) {
+        // If grouping timestamps from a multi-trip dataset,
+        // do not create pairs over differing trips,
+        // since the vehicles are different.
+        latestGroup.timestamps.push(currentTimestamp);
+    }
+
+    timestampGroups.push({
+        tripId: currentTimestamp.tripId,
+        doorsOpen: currentTimestamp.doorsOpen,
+        timestamps: [currentTimestamp],
+    });
 
     return timestampGroups;
 }
 
-function mapTimestampGroupDurationWithDoorStatus(timestampGroup) {
-    const [firstSeen] = timestampGroup.timestamps;
-    const [lastSeen] = timestampGroup.timestamps.slice().reverse();
+/**
+ * Remove closed-door timestamp groups,
+ * we only care how long the doors are open
+ * since that's what affects congestion.
+ *
+ * @param {Object} timestampPair
+ * @returns {boolean}
+ */
+function removeClosedDoorTimestampGroupsFilter(timestampPair) {
+    return timestampPair.doorsOpen === true;
+}
 
-    const groupDurationInSeconds = moment(lastSeen.seenAtStop)
+/**
+ * Get time diff between group of timestamps.
+ *
+ * @param {Object} timestampGroup
+ * @returns {{durationInSeconds: number, doorsOpen: boolean}} Door status with duration
+ */
+function timestampGroupToDoorStatusDurationMapper(timestampGroup) {
+    const [firstSeen] = timestampGroup.timestamps;
+    const [lastSeen] = timestampGroup.timestamps.slice(-1);
+
+    const pairDurationInSeconds = moment(lastSeen.seenAtStop)
         .diff(moment(firstSeen.seenAtStop), 'seconds');
 
     return {
         doorsOpen: timestampGroup.doorsOpen,
-        durationInSeconds: groupDurationInSeconds,
+        durationInSeconds: pairDurationInSeconds,
     };
 }
 
-function sumTimestampGroupDurationsReducer(totalDurationInSeconds, timestampGroup) {
-    return totalDurationInSeconds + timestampGroup.durationInSeconds;
+/**
+ * Sum time diffs of door status durations.
+ *
+ * @param {number} totalDurationInSeconds
+ * @param {{durationInSeconds: number, doorsOpen: boolean}} doorStatusDuration Door status with duration
+ * @returns {number}
+ */
+function sumDoorStatusDurationsReducer(totalDurationInSeconds, doorStatusDuration) {
+    return totalDurationInSeconds + doorStatusDuration.durationInSeconds;
 }
 
 function getTimestampsLogByTrip(stopId, tripId) {
